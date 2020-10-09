8cacbe8f31578d3760093d488f97baa49392ba22
Benjamin Rosen <ben@classy.name>
2020-10-09 17:38:38 -0400

    sketch getInstanceId function

--- src/function/getInstanceId.js
+++ src/function/getInstanceId.js
@@ -1,6 +1,15 @@
-async function getInstanceId() {
-  // TODO: get instance id from aws ec2 metadata
-  return "12345";
+const AWS = require("aws-sdk");
+
+const metadataService = new AWS.MetadataService();
+
+function getInstanceId() {
+  return metadataService
+    .request("/latest/meta-data/instance-id")
+    .promise()
+    .then((data) => {
+      console.log(data);
+      return "beep boop";
+    });
 }
 
 module.exports = { getInstanceId };
